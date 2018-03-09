eb6843350974a662985faa507a48b4914220ca8c
Vk <kunwanjian@qq.com>
2018-03-09 17:41:30 +0800

    增加 获取设备类型的 api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\DevModel;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
@@ -24,6 +25,12 @@ class Controller extends BaseController
         // `customer_id`,`customer_name`
         $id = $request->get('q');
         return Customers::where('sh_name_id',$id)->get([DB::raw('customer_id as id'),DB::raw('customer_name as text')]);
+    }
+
+    public function dev_model(Request $request){
+        // `customer_id`,`customer_name`
+        $id = $request->get('q');
+        return DevModel::where('dev_model_id',$id)->get([DB::raw('dev_model_id as id'),DB::raw('dev_model_name as text')]);
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return \App\DevModel::all();
 });
-
+// 所有商户
 Route::get('/allSHInfos', 'Controller@allSHInfos');
-
+/// 通过商户获取客户
 Route::get('/customer', 'Controller@customer');
+/// 通过客户获取产品类型
+Route::get('/dev_model', 'Controller@dev_model');
