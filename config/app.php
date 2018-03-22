35ee15862b2d66a2c54e26f1d2e94e4ecdc17d63
Vk <kunwanjian@qq.com>
2018-03-22 16:40:09 +0800

    gitlab ci

--- config/app.php
+++ config/app.php
@@ -166,6 +166,11 @@ return [
          */
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
+        /**
+        * Package Service Providers...
+        */
+        Maatwebsite\Excel\ExcelServiceProvider::class,
+
     ],
 
     /*
@@ -214,6 +219,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
