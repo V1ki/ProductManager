9cc69a963cde60fe77c0f2a3d2fc2ab7de1ad66d
Vk <kunwanjian@qq.com>
2018-03-30 11:16:29 +0800

    通过订单号 创建设备， 并返回设备信息。

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -57,11 +57,85 @@ class Controller extends BaseController
     public function createDevice(Request $request) {
         // 订单号
         $orderNo = $request->get('order');
+        $order = Order::where('number',$orderNo)->first();
+        if( $order == null) {
+            return "23";
+        }
 
-        $order = Order::where('number',$orderNo);
+
+        $device = Device::create([
+            'sn' => 'sn_test',
+            'order_id' => $orderNo,
+            'soft_version' => $order->soft_version,
+            'hardware_version' => $order->hardware_version,
+            'dev_model_id' => $order->dev_model_id,
+            'imei1' => str_pad_dechex_0($order->imei_start + 0,15) ,
+            //'imei2' => $imei_start,
+            'mac_wifi' => $this->generateMac($order->mac_wifi_start, 1),
+            'mac_bluetooth' => $this->generateMac($order->mac_bluetooth_start,1),
+        ]);
+
+
         // 通过订单号 生成
-        return $order;
+        return $device ;
 
     }
 
+
+    private function generateMac($start, $index = 0)
+    {
+        $nums = explode(":", $start);
+        if ($nums || $nums->sum != 4) {
+            $nums = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+        }
+
+        // 取出 每一位 并转换成 十进制
+        $first = hexdec($nums[0]);
+        $second = hexdec($nums[1]);
+        $third = hexdec($nums[2]);
+        $four = hexdec($nums[3]);
+        $five = hexdec($nums[4]);
+        $last = hexdec($nums[5]);
+
+        // 大于 最大值 则取下一位、
+        if (($last + $index) > 255) {
+            $last = 255;
+
+            // 大于 最大值 则取下一位、
+            if (($third + $index) > 255) {
+                $third = 255;
+
+                // 大于 最大值 则取下一位、
+                if (($second + $index) > 255) {
+                    $second = 255;
+
+                    // 大于 最大值 则取下一位、
+                    if (($first + $index) > 255) {
+                        $first = 255;
+                    } else {
+                        $first = $first + $index;
+                    }
+                } else {
+                    $second = $second + $index;
+                }
+            } else {
+                $third = $third + $index;
+            }
+
+        } else {
+            $last = $last + $index;
+        }
+
+
+        return str_pad_dechex_0($first) . ':' . str_pad_dechex_0($second) . ':' . str_pad_dechex_0($third) . ':'. str_pad_dechex_0($four) . ':'. str_pad_dechex_0($five) . ':' . str_pad_dechex_0($last);
+
+    }
+
+
+
+}
+
+function str_pad_dechex_0($input, $length = 2, $pad_type = STR_PAD_LEFT)
+{
+    return str_pad(dechex($input), $length, "0", $pad_type);
 }
